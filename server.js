335010656c8ff3084984dd99211bdf031320eae7
Fábio <sitefabiogurgel@gmail.com>
2025-12-24 00:47:10 -0300

    CONTACTESTtwo

--- server.js
+++ server.js
@@ -87,3 +87,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
+app.use((req, res) => {
+  res.redirect("/");
+});
