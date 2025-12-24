bb07315d536b494caefccff02c50f9f15f881ea3
Fábio <sitefabiogurgel@gmail.com>
2025-12-23 23:47:52 -0300

    RELEASE

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ conectarMongo();
 
 //  ROTAS HTML
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/menu.html"));
+  res.sendFile(path.join(__dirname, "Menu/index.html"));
 });
 
 app.get("/menu", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/menu.html"));
+  res.sendFile(path.join(__dirname, "Menu/index.html"));
 });
 
 app.get("/contato", (req, res) => {
